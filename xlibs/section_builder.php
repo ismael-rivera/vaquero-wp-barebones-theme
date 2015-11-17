5fedebda39a3ebdc4ad8cdb7e93c0b68224e2ef3
Ismael Ishy Rivera <coralblue79@gmail.com>
2015-11-17 04:30:17 -0200

    Modifications MEBUI HELPER CLASS

--- xlibs/section_builder.php
+++ xlibs/section_builder.php
@@ -1,7 +1,7 @@
 <?php
 /*
 *
-* MEBUI_HELPER CLASS V0.13
+* MEBUI_HELPER CLASS V0.16
 * Author: Ismael Rivera
 * website: speggo.com
 * email: speggodesign@gmail.com 
@@ -14,32 +14,66 @@
 * Includes pre-tags and commentary that can be customized to retrofit the output.
 * 
 */
-
+/*
+	* name:string  = This would be the name you want to give to the section you're creating.
+	* location:int = (values 1,2 or 3) In the event that the developer wishes to specify 
+	*                the location on the page where the section is being created as either 
+					 part of the header(1), the content(2) or the footer(3).
+	  section_grid:bool  = Basically, Are you going to use a grid?
+	  spacer_content = If you want to create a spacer div between sections, just add some 
+	  				   content to this index or leave it blank(not NULL which is different), 
+					   but blank and a spacer div will be created on top of the created 
+					   section. 			 
+	* grouping:int = Unlike location which is a sort of grouping, this is more like a 
+					 categorization of sections under a specific subject. This can be used for
+					 a number of applications including plugin development, auto-sorting, tagging
+					 etc, etc.
+	* spacer_classes = I know I wrote this index option for a reason. Right now I have no 
+					   recollection of it's purpose.
+	* content_wrapper_classes = ditto. Still working this out.
+	*/
+	
 class Section_Builder{
 	
 	public static $name;
 	public static $section;
 	public static $options;
 	
-	public static function sec_Begins($name=NULL, $section=NULL, $options = array( 'section_grid' => true, 
-	'spacer_content' => NULL, 'section_grouping' => NULL, 'spacer_classes' => array(), 'content_wrapper_classes' => array())){
+	/*
+	Call this way:
+	Section_Builder::sec_Begins($args);
+	*/
 	
-	//for content end sectiion	
-	self::$name = $name;
+	public static function sec_Begins($options = array( 'name' => NULL, 
+														'location' => NULL, 
+														'section_grid' => true, 
+														'spacer_content' => NULL, 
+														'grouping' => NULL, 
+														'spacer_classes' => array(), 
+														'content_wrapper_classes' => array()
+														)
+									  )
+	{
+	
+	
+		
+	self::$name = $options['name'];
 	self::$options = $options;
 		
 	if (isset($options['section_grouping'])){	
 	$in  ='<!--====BEGIN/SECTION '.strtoupper($options['section_grouping']).'/HERE====-->'."\n";
 	} else {
-	$in  ='<!--==================================================BEGIN/SECTION/HERE=============================================-->'."\n";	
+	$in  ='<!--==================================================';
+	$in .='BEGIN/SECTION/HERE';
+	$in .='==================================================-->'."\n";	
 	}
 	$in .='<!--Cogratulations: You have just constructed a section for this homepage-->'."\n";
-	$in .='<!--SECTION '.strtoupper($name).' HAS NOW BEEN CREATED-->'."\n";
-	$in .= '<span id="stopper_'.$name.'" class="easing_stopper"></span>'."\n";
+	$in .='<!--SECTION '.strtoupper($options['name']).' HAS NOW BEEN CREATED-->'."\n";
+	$in .= '<span id="stopper_'.$options['name'].'" class="easing_stopper"></span>'."\n";
 	if(isset($options['spacer_content'])){
-	    $in .= '<div id="spacer_content_wrapper_'.$name.'" class="wrapper">'. $options['spacer_content'] .'</div>';
+	    $in .= '<div id="spacer_content_wrapper_'.$options['name'].'" class="wrapper">'. $options['spacer_content'] .'</div>';
 	} else {
-	    $in .= '<div id="spacer_wrapper_'.$name.'" class="wrapper';
+	    $in .= '<div id="spacer_wrapper_'.$options['name'].'" class="wrapper';
 		if(isset($options['spacer_classes'])){
 			if (is_array($options['spacer_classes'])){
 				foreach ($options['spacer_classes'] as $spacer_class){
@@ -51,7 +85,7 @@ class Section_Builder{
 		}
 	    $in .= '"></div>'."\n";
 	}
-	$in .= '<div id="content_wrapper_'.$name.'" class="wrapper';
+	$in .= '<div id="content_wrapper_'.$options['name'].'" class="wrapper';
 	
 	if(isset($section)){
 		if (is_int($section)){
@@ -83,9 +117,9 @@ class Section_Builder{
 	$in .= '">'."\n";
 	if ($options['section_grid'] == true){
 
-	$in .= '<div id="section_'.$name.'" >'."\n\n";	
+	$in .= '<div id="section_'.$options['name'].'" >'."\n\n";	
 		} else {
-	$in .= '<div id="section_'.$name.'" class="grid">'."\n\n";
+	$in .= '<div id="section_'.$options['name'].'" class="grid">'."\n\n";
 		}
 	$in .= '<!--######### Section Content BEGIN #########-->'."\n\n";
 	
